b8a94e538cf4ac42b503657df55537e18df19fdf
Matuku45 <150783819+Matuku45@users.noreply.github.com>
2025-06-26 20:00:50 +0200

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import PersonListCreate, PersonUpdateDelete
 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
 urlpatterns = [
     path('people/', PersonListCreate.as_view(), name='people'),
     path('people/<int:person_id>/', PersonUpdateDelete.as_view(), name='person-detail'),
