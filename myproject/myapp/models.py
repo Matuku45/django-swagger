07e82df2af4895149bc5f7bd2d64aac8d16b49ce
Manuel Mapoulo <150783819+Matuku45@users.noreply.github.com>
2025-08-15 21:26:44 +0200

    Update models.py

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class PersonModel(models.Model):
     name = models.CharField(max_length=200)
-    lastname = models.CharField(max_length= 300)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # hashed password
+    agree_terms = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
